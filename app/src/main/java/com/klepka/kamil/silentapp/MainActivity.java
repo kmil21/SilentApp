11604be09ad0e4872f5a95cd811350a6cbbe027c
Kamil Klepka <kamil.klepka@gmail.com>
2016-03-10 23:11:22 +0100

    Copy project

---
+++ app/src/main/java/com/klepka/kamil/silentapp/MainActivity.java
@@ -0,0 +1,102 @@
+package com.klepka.kamil.silentapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.touchboarder.weekdaysbuttons.WeekdaysDataItem;
+import com.touchboarder.weekdaysbuttons.WeekdaysDataSource;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WeekdaysDataSource weekdaysDataSource;
+    Toolbar toolbar,toolbar2;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       toolbar2=(Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar2);
+       getSupportActionBar().setTitle("Dodaj nowy");
+       // getSupportActionBar().setIcon(R.drawable.ic_add_alarm_black_48dp);
+        intent = new Intent(this, SilentPicker.class);
+        toolbar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Toolbar title clicked", Toast.LENGTH_SHORT).show();
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       // startActivityForResult(new Intent(this, SilentPicker.class), 1);
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+       getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/klepka/kamil/silentapp/SilentPicker.java
@@ -0,0 +1,188 @@
+package com.klepka.kamil.silentapp;
+
+
+import android.content.res.Configuration;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.touchboarder.weekdaysbuttons.WeekdaysDataItem;
+import com.touchboarder.weekdaysbuttons.WeekdaysDataSource;
+import com.touchboarder.weekdaysbuttons.WeekdaysDrawableProvider;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by Kamil on 2016-03-09.
+ */
+public class SilentPicker extends AppCompatActivity implements WeekdaysDataSource.Callback {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private boolean is24Hour = true,VibOn=false;
+    private WeekdaysDataSource weekdaysDataSource;
+    private float LargeScaleX = 1.5f,LargeScaleY = 1.5f,NormalScaleX =1f,NormalScaleY=1f;
+    private float LargeTxt = 23f,NormalTxt=18f;
+    private TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_silent);
+        NestedScrollView scrollView = (NestedScrollView) findViewById(R.id.nest_scrollview);
+        scrollView.setFillViewport(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Sprawdzić oprogramowywanie tego guzika
+
+        TimePicker start = (TimePicker) findViewById(R.id.timePickerStart);
+        start.setIs24HourView(is24Hour);
+        TimePicker end = (TimePicker) findViewById(R.id.timePickerEnd);
+       end.setIs24HourView(is24Hour);
+
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        String toastMsg;
+        switch(screenSize) {
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                toastMsg = "Large screen";
+                start.setScaleX(LargeScaleX);
+                start.setScaleY(LargeScaleY);
+                end.setScaleX(LargeScaleX);
+                end.setScaleY(LargeScaleY);
+                txt = (TextView)findViewById(R.id.bar1);
+                txt.setTextSize(LargeTxt);
+                txt = (TextView)findViewById(R.id.bar2);
+                txt.setTextSize(LargeTxt);
+                txt = (TextView)findViewById(R.id.bar3);
+                txt.setTextSize(LargeTxt);
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                toastMsg = "Normal screen";
+                start.setScaleX(NormalScaleX);
+                start.setScaleY(NormalScaleY);
+                end.setScaleX(NormalScaleX);
+                end.setScaleY(NormalScaleY);
+                txt = (TextView)findViewById(R.id.bar1);
+                txt.setTextSize(NormalTxt);
+                txt = (TextView)findViewById(R.id.bar2);
+                txt.setTextSize(NormalTxt);
+                txt = (TextView)findViewById(R.id.bar3);
+                txt.setTextSize(NormalTxt);
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                toastMsg = "Small screen";
+                break;
+            default:
+                toastMsg = "Screen size is neither large, normal or small";
+        }
+
+        if (savedInstanceState != null) {
+            // Restore the weekdaysDataSource state, save a reference to weekdaysDataSource.
+
+            weekdaysDataSource = WeekdaysDataSource.restoreState("wds1", savedInstanceState, this, callback1, null);
+        }
+        else {
+            // No previous state, first creation.
+            setupWeekdaysButtons();
+
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        if (weekdaysDataSource != null)
+        {
+            weekdaysDataSource.saveState("wds1",outState);
+        }
+    }
+    public void checkAll(View v) {
+        switch (v.getId()) {
+            case R.id.check_all:
+                weekdaysDataSource.selectAll(((CheckBox) v).isChecked());
+                break;
+
+        }
+    }
+    public void checkFill(View v) {
+        switch (v.getId()) {
+            case R.id.check_fill:
+                weekdaysDataSource.setFillWidth(((CheckBox) v).isChecked());
+                break;
+        }
+    }
+
+    public void setVibr (View v)
+    {
+
+    }
+    public void setupWeekdaysButtons() {
+        weekdaysDataSource  = new WeekdaysDataSource(this, R.id.weekdays_stub)
+                .setDrawableType(WeekdaysDrawableProvider.MW_ROUND_RECT)
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setFontBaseSize(16)
+                .setFontTypeFace(Typeface.MONOSPACE)
+                .setUnselectedColorRes(R.color.colorPrimaryLight)
+                .setTextColorUnselectedRes(R.color.colorSecondaryText)
+                .setTextColorSelectedRes(R.color.black)
+                .setNumberOfLetters(3)
+                .setFillWidth(true)
+
+                             .start(callback1);
+    }
+
+    private WeekdaysDataSource.Callback callback1 = new WeekdaysDataSource.Callback() {
+        @Override
+        public void onWeekdaysItemClicked(int attachId,WeekdaysDataItem item) {
+            CheckBox checkBox = (CheckBox) findViewById(R.id.check_all);
+            if (checkBox != null) checkBox.setChecked(weekdaysDataSource.isAllDaysSelected());
+
+            Calendar calendar = Calendar.getInstance();
+            int today = calendar.get(Calendar.DAY_OF_WEEK);
+            if(item.getCalendarDayId()==today&&item.isSelected())
+                Toast.makeText(SilentPicker.this,"Carpe diem",Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onWeekdaysSelected(int attachId,ArrayList<WeekdaysDataItem> items) {
+
+        }
+    };
+
+    @Override
+    public void onWeekdaysItemClicked(int attachId,WeekdaysDataItem item) {
+
+
+        //Filter on the attached id if there is multiple weekdays data sources
+        if(attachId==R.id.weekdays_recycler_view){
+            Calendar calendar = Calendar.getInstance();
+            int today = calendar.get(Calendar.DAY_OF_WEEK);
+            if(item.getCalendarDayId()==today&&item.isSelected())
+                Toast.makeText(SilentPicker.this,"Carpe diem",Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onWeekdaysSelected(int attachId,ArrayList<WeekdaysDataItem> items) {
+//       String selectedDays=getSelectedDaysFromWeekdaysData(items);
+//        if(!TextUtils.isEmpty(selectedDays))
+//            showSnackbarShort(selectedDays);
+    }
+    public void TimeSilentSet (View view)
+    {
+        finish();
+    }
+
+
+}
